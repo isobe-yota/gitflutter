58bd1926613b014dbbc1668eb5ee1ab0f970d5a9
PC235\isobe.yota <isobe.yota@opusstudio.com>
2022-04-22 17:53:23 +0900

    カウント数で表示を更新

--- flutter/test1/lib/main.dart
+++ flutter/test1/lib/main.dart
@@ -49,15 +49,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  String _type = "偶数";
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      if (_counter % 2 == 0) {
+        _type = "偶数";
+      } else {
+        _type = "奇数";
+      }
     });
   }
 
@@ -65,14 +65,28 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Row(children: [
-        Icon(Icons.create),
-        Text("初めてのタイトル"),
-      ])),
-      drawer: Drawer(child: Center(child: Text("Drawer"))),
-      body: Text("初めてのテキスト"),
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text('$_type', style: TextStyle(fontSize: 20, color: Colors.red))
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () => {print("押したね？")}, child: Icon(Icons.timer)),
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
