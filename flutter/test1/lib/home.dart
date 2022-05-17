cd4f6aa3be2e80e1a1051464d39162c63a06e3b4
PC235\isobe.yota <isobe.yota@opusstudio.com>
2022-05-17 18:40:15 +0900

    ログイン、ログアウト処理を導入

--- flutter/test1/lib/home.dart
+++ flutter/test1/lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sensors/sensors.dart';
+import 'dart:async';
 
 // [Themelist] インスタンスにおける処理。
 class Home extends StatelessWidget {
@@ -10,18 +12,15 @@ class Home extends StatelessWidget {
 
   // 前画面から受け取った値はNull許容のため、入れ直し用の変数を用意
   late String _user_id;
-
   @override
   Widget build(BuildContext context) {
     _user_id = user_id ?? 'ログインユーザー名取得失敗';
-
-    const List<String> _popmenu_list = ["テスト", "ログアウト"];
+    const List<String> _popmenu_list = ["テストアプリ", "ログアウト"];
 
     return Scaffold(
-      // Header部分
       appBar: AppBar(
         leading: Icon(Icons.home),
-        title: Text('ログイン後の画面'),
+        title: Text("testhome"),
         backgroundColor: Colors.black87,
         centerTitle: true,
         elevation: 0.0,
@@ -33,7 +32,7 @@ class Home extends StatelessWidget {
             icon: Icon(Icons.menu),
             onSelected: (String s) {
               if (s == 'ログアウト') {
-                auth!.signOut();
+                auth?.signOut();
                 Navigator.of(context).pushNamed("/login");
               }
             },
@@ -48,15 +47,105 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
+      body: MyHomePage(),
+    );
+  }
+}
 
-      // メイン画面
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  List<double> accelerometerValues = <double>[];
+  List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addObserver(this);
+    _streamSubscriptions
+        .add(accelerometerEvents.listen((AccelerometerEvent event) {
+      setState(() {
+        accelerometerValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+  }
+
+  @override
+  void dispose() {
+    print("dispose");
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print("stete = $state");
+    switch (state) {
+      case AppLifecycleState.inactive:
+        print('非アクティブになったときの処理');
+        break;
+      case AppLifecycleState.paused:
+        print('停止されたときの処理');
+        break;
+      case AppLifecycleState.resumed:
+        print('再開されたときの処理');
+        break;
+      case AppLifecycleState.detached:
+        print('破棄されたときの処理');
+        break;
+    }
+  }
+
+  int _counter = 0;
+  int _roopcount = 0;
+
+  _incrementCounter(bool flag) {
+    if (flag) {
+      _roopcount = 1;
+    } else {
+      if (_roopcount == 1) {
+        setState(() {
+          _counter++;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double accel = accelerometerValues[0] +
+        accelerometerValues[1] +
+        accelerometerValues[2];
+
+    if (accel > 20.0) {
+      _incrementCounter(true);
+    } else if (accel < 5.0) {
+      _incrementCounter(false);
+    }
+
+    const List<String> _popmenu_list = ["テストアプリ", "ログアウト"];
+
+    return Scaffold(
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('ようこそ',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            Text(_user_id),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("ACCEL: $accel"),
+            Text(
+              '歩いた歩数',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
           ],
         ),
       ),
